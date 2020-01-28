1808c2edbd8b6e8aa2000d8d0252fe2114b21800
Jack Domleo <jack.domleo@mitrefinch.co.uk>
2020-01-28 12:43:31 +0000

    Build website documentation docs

---
+++ docs/precache-manifest.63c9a3b8a4f1a6158f3cd778fa793656.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22daaacfbbf9a922e6ed0ae03d2f9bfc",
+    "url": "/CNAME"
+  },
+  {
+    "revision": "0ffa73df3891b8badc41",
+    "url": "/css/app.65b8beac.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "595b64d516b526f8028b7a9a18f07b0f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3064474e82990937686a",
+    "url": "/js/about.cd97635f.js"
+  },
+  {
+    "revision": "0ffa73df3891b8badc41",
+    "url": "/js/app.383ce182.js"
+  },
+  {
+    "revision": "07cdcf620f06039acc52",
+    "url": "/js/chunk-vendors.da35c094.js"
+  },
+  {
+    "revision": "c08e22d86e1ec61aa5eaede5f042f1bd",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
