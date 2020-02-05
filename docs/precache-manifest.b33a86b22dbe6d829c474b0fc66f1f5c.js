0fd85496477593847f8c3ae71efc079ad057c53c
Jack Domleo <jackdomleo7@hotmail.com>
2020-02-05 22:38:28 +0000

    npm run build

---
+++ docs/precache-manifest.b33a86b22dbe6d829c474b0fc66f1f5c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22daaacfbbf9a922e6ed0ae03d2f9bfc",
+    "url": "/CNAME"
+  },
+  {
+    "revision": "fe16a8732fd6568c377a",
+    "url": "/css/app.37d10bef.css"
+  },
+  {
+    "revision": "0a9fc194e19085d2afd5",
+    "url": "/css/chunk-882fe588.e75466d7.css"
+  },
+  {
+    "revision": "3955e2d9d2d6c564f1b37e53a92becb8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe16a8732fd6568c377a",
+    "url": "/js/app.68ae748c.js"
+  },
+  {
+    "revision": "ccf8595a1db60865ed00",
+    "url": "/js/chunk-2d22d746.5db97e80.js"
+  },
+  {
+    "revision": "0a9fc194e19085d2afd5",
+    "url": "/js/chunk-882fe588.b17355ef.js"
+  },
+  {
+    "revision": "df4232282f983b4c2426",
+    "url": "/js/chunk-vendors.15a04a54.js"
+  },
+  {
+    "revision": "c08e22d86e1ec61aa5eaede5f042f1bd",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
