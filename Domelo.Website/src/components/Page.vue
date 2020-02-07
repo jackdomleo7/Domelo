369182c33465b7a0834746729b5df29b121fc504
Jack Domleo <jack.domleo@mitrefinch.co.uk>
2020-02-07 16:05:17 +0000

    Documented color and started typography

--- Domelo.Website/src/components/Page.vue
+++ Domelo.Website/src/components/Page.vue
@@ -1,11 +1,12 @@
 <template>
   <main class="page">
-    <h1 class="page__heading">
+    <h1 class="heading--l page__heading">
       <slot name="pageTitle" />
     </h1>
-    <p class="page__description">
+    <p class="body--m page__description">
       <slot name="pageDescription" />
     </p>
+    <slot />
     <router-view />
   </main>
 </template>
@@ -18,8 +19,6 @@ export default class Page extends Vue {}
 </script>
 
 <style lang="scss" scoped>
-@import "../Domelo.Styles/scss/typography/mixins";
-
 .page {
   max-width: 68rem;
   padding: 1rem 2rem;
@@ -30,13 +29,11 @@ export default class Page extends Vue {}
   }
 
   &__heading {
-    @include heading(var(--font-size-500));
     margin-top: 0;
   }
 
   &__description {
-    @include body(var(--font-size-100));
-    font-weight: 300;
+    font-weight: var(--font-weight-light);
   }
 }
 </style>
