e3733c479c34eb396325cad5ae4408da65d3d796
Jack Domleo <jack.domleo@mitrefinch.co.uk>
2020-02-09 23:46:45 +0000

    Added named routes and started documenting object styles

--- Domelo.Website/src/router/index.ts
+++ Domelo.Website/src/router/index.ts
@@ -3,35 +3,50 @@ import VueRouter from 'vue-router';
 
 Vue.use(VueRouter);
 
+export class Routes {
+  public static readonly Home: string = 'home';
+  public static readonly FoundationColor: string = 'foundation-color';
+  public static readonly FoundationGrid: string = 'foundation-grid';
+  public static readonly FoundationObjectStyles: string = 'foundation-object-styles';
+  public static readonly FoundationPlatformScale: string = 'foundation-platform-scale';
+  public static readonly FoundationResetStylesheet: string = 'foundation-reset-stylesheet';
+  public static readonly FoundationTypography: string = 'foundation-typography';
+}
+
 const routes = [
   {
     path: '/',
-    name: 'home',
+    name: Routes.Home,
     component: () => import('@/views/Home.vue'),
   },
   {
     path: '/foundation/color',
-    name: 'color',
+    name: Routes.FoundationColor,
     component: () => import('@/views/foundation/Color.vue'),
   },
   {
     path: '/foundation/grid',
-    name: 'grid',
+    name: Routes.FoundationGrid,
     component: () => import('@/views/foundation/Grid.vue'),
   },
   {
-    path: '/foundation/reset-stylesheet',
-    name: 'reset-stylesheet',
-    component: () => import('@/views/foundation/Reset.vue'),
+    path: '/foundation/object-styles',
+    name: Routes.FoundationObjectStyles,
+    component: () => import('@/views/foundation/ObjectStyles.vue'),
   },
   {
     path: '/foundation/platform-scale',
-    name: 'platform-scale',
+    name: Routes.FoundationPlatformScale,
     component: () => import('@/views/foundation/Scale.vue'),
   },
   {
+    path: '/foundation/reset-stylesheet',
+    name: Routes.FoundationResetStylesheet,
+    component: () => import('@/views/foundation/Reset.vue'),
+  },
+  {
     path: '/foundation/typography',
-    name: 'typography',
+    name: Routes.FoundationTypography,
     component: () => import('@/views/foundation/Typography.vue'),
   },
 ];
---
+++ Domelo.Website/src/views/foundation/ObjectStyles.vue
@@ -0,0 +1,22 @@
+<template>
+  <page data-page="object-styles">
+    <template #pageTitle>Object styles</template>
+    <template #pageDescription>A set of global styles to apply to components.</template>
+    <article id="rounding">
+      <h2 class="heading--l">Rounding</h2>
+    </article>
+    <article id="box-shadow">
+      <h2 class="heading--l">Box shadow</h2>
+    </article>
+  </page>
+</template>
+
+<script lang="ts">
+  import {Component, Vue} from 'vue-property-decorator';
+  import Page from '@/components/Page.vue';
+
+  @Component({
+    components: { Page },
+  })
+  export default class ObjectStyles extends Vue {}
+</script>
