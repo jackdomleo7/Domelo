f9da0b435c050472cb1e16e57f0dd1962097fba4
Jack Domleo <jack.domleo@mitrefinch.co.uk>
2020-02-13 19:14:53 +0000

    Fixed docs

--- Domelo.Website/src/views/foundation/DesignTokens.vue
+++ Domelo.Website/src/views/foundation/DesignTokens.vue
@@ -5,10 +5,11 @@
         <h2 class="heading--l">Design token types</h2>
         <h3 class="heading--subheading">Global tokens</h3>
         <p class="body--m">Global tokens store the raw values of the Domelo design system. E.g. Colors, font sizes, font weights, animation timings, etc.</p>
-        <h3 class="heading--l">Alias tokens</h3>
+        <h3 class="heading--subheading">Alias tokens</h3>
         <p class="body--m">Alias tokens are the connection between global tokens and component tokens. They can often represent values that will be used more than once. E.g. Call to action colors, error/success colors, etc.</p>
         <h3 class="heading--subheading">Component tokens</h3>
         <p class="body--m">Component tokens are specific to a component.</p>
+        <input type="checkbox" />
     </page>
 </template>
 
