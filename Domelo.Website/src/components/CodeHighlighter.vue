0b5016d9152cd9d5a57f33fc4b1dbe954e391702
Jack Domleo <jackdomleo7@hotmail.com>
2020-03-16 22:31:23 +0000

    PoC with Prism

---
+++ Domelo.Website/src/components/CodeHighlighter.vue
@@ -0,0 +1,25 @@
+<template>
+    <prism-editor
+        v-model="code"
+        language="html"
+        line-numbers
+        readonly
+        class="my-editor"
+    />
+</template>
+
+<script lang="ts">
+  import { Component, Prop, Vue } from 'vue-property-decorator';
+  import PrismEditor from "vue-prism-editor";
+  import "prismjs";
+  import "prismjs/themes/prism-tomorrow.css";
+  import "vue-prism-editor/dist/VuePrismEditor.css";
+
+  @Component({
+    components: { PrismEditor },
+  })
+  export default class CodeHighlighter extends Vue {
+    @Prop({ type: String, default: "" })
+    private readonly code!: string;
+  }
+</script>
